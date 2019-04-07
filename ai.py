d60090cfb844e995309a490507aa79acf0de290c
yisaketsegaye <36218240+yisaketsegaye@users.noreply.github.com>
2019-04-07 16:15:50 -0400

    count_neuron fun

--- ai.py
+++ ai.py
@@ -168,19 +168,21 @@ class CNN(nn.Module):
     
     def __init__(self, number_actions):
         super(CNN,self).__init__()
-        self.convolutionl1 = nn.Conv2d(in_channels = 1 out_channel , kernal_size)
-        self.convolutionl2
-        self.convolution3
-        self.fc1
-        self.fc2
+        self.convolutionl1 = nn.Conv2d(in_channels = 1, out_channel = 32 , kernel_size = 5)
+        self.convolutionl2 = nn.Conv2d(in_channels = 32, out_channel = 32 , kernel_size = 3)
+        self.convolution3 = nn.Conv2d(in_channels = 1, out_channel = 64 , kernel_size = 2)
+        self.fc1 = nn.Linear(in_features = self.count_neurons(1,80,80) , out_features = 40 )
+        self.fc2 = nn.Linear(in_features = 40  , out_features = number_actions )
         
         
 
 
-
-
-
-
+    def count_neurons(self, image_dim):
+        x= Variable(torch.rand(1, *image_dim))
+        x= F.relu(F.max_pool2d(self.convolution1(x), 3,2))
+        x= F.relu(F.max_pool2d(self.convolution2(x), 3,2))
+        x= F.relu(F.max_pool2d(self.convolution3(x), 3,2))
+        return x.data.view(1,-1).size(1)
 
 
 
